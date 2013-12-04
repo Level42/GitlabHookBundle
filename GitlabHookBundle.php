c5f4166d2cca49b4b2f52d96495cf1104f3914cc
Florent PERINEL <florent@redfroggy.fr>
2013-12-04 23:33:52 +0000

    Ajout tests et travis

---
+++ GitlabHookBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the GitlabHookBundle package
+ *
+ * (c) Level42 <level42.dev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Level42\GitlabHookBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle main class
+ */
+class GitlabHookBundle extends Bundle
+{
+}
---
+++ Tests/bootstrap.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * This file is part of the GitlabHookBundle package
+ *
+ * (c) Level42 <level42.dev@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+$loader = @include __DIR__ . '/../vendor/autoload.php';
+if (!$loader) {
+    die(
+            <<<'EOT'
+You must set up the project dependencies, run the following commands:
+wget http://getcomposer.org/composer.phar
+php composer.phar install
+EOT
+    );
+}
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader(
+        array($loader, 'loadClass'));
+
+spl_autoload_register(
+        function ($class)
+        {
+            if (0 === strpos($class, 'Level42\\GitlabHookBundle\\')) {
+                $path = __DIR__ . '/../'
+                        . implode('/', array_slice(explode('\\', $class), 2))
+                        . '.php';
+                if (!stream_resolve_include_path($path)) {
+
+                    return false;
+                }
+                require_once $path;
+
+                return true;
+            }
+        });
+
+// Chargement du kernel
+require_once __DIR__ . '/app/GitlabHookTestKernel.php';
